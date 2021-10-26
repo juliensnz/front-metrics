b5e918afcdb63736af50da8c4d2bc523b12b996a
Steven VAIDIE <steven.vaidie@gmail.com>
2021-10-26 14:36:27 +0200

    HANDLEMULTIREPORT: WIP

--- bin/console.mjs
+++ bin/console.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
-const fs = require('fs');
-
-const {computeReport} = require('../src/cli/computeReport');
+import yargs from 'yargs';
+import fetch from 'node-fetch';
+import fs from 'fs';
+import {computeReport} from '../src/cli/computeReport.js';
 
 const {_: commandName, folderToAnalyze, reportName, $0: binaryPath} = yargs
   .command('report:generate [folderToAnalyze] [reportName]', 'Generate a new report to be anlyzed', yargs => {
@@ -36,15 +36,27 @@ const {_: commandName, folderToAnalyze, reportName, $0: binaryPath} = yargs
   .alias('h', 'help')
   .demandCommand(2).argv;
 
-
 (async () => {
   try {
     switch (commandName[0]) {
       case 'report:generate':
         const report = computeReport(folderToAnalyze);
-
         fs.writeFileSync(`./public/reports/${reportName}.json`, JSON.stringify(report))
+        if (true) {
+//      if (process.env.CI === 'true') {
+          const response = await fetch('https://api.github.com/repos/juliensnz/front-metrics/contents/reports?ref=gh-pages');
+          const reportResponse = await response.json();
 
+          const reportFiles = reportResponse.filter((report) => !['.gitkeep', 'reports.json'].includes(report.name));
+          await Promise.all(reportFiles.map(async (reportFile) => {
+            const reportContent = await fetch(reportFile.download_url);
+
+            fs.writeFileSync(`./public/reports/${reportFile.name}`, await reportContent.text());
+          }));
+        }
+
+          // Aggregé les données dans reports.json
+          //date +%F
         const files = fs.readdirSync('./public/reports/');
         fs.writeFileSync(`./public/reports/reports.json`, JSON.stringify(
           files.filter(path => path.includes('.json') && path !== 'reports.json')
